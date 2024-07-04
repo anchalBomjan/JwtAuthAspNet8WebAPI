3fa970419d4210d4d576e28d71855223840fdfd7
PERCOID-IT <PERCOID-IT@DESKTOP-SANISHA>
2024-07-04 13:47:57 +0545

    Add Dto and static class#

---
+++ JwtAuthAspNet8WebAPI/Data/Dto/RegisterDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthAspNet8WebAPI.Data.Dto
+{
+    public class RegisterDto
+    {
+        //[Required(ErrorMessage = "FirstName is required")]
+        //public string FirstName { get; set; }
+
+        //[Required(ErrorMessage = "LastName is required")]
+        //public string LastName { get; set; }
+
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+    }
+}
---
+++ JwtAuthAspNet8WebAPI/Data/Dto/UpdatePermissionDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthAspNet8WebAPI.Data.Dto
+{
+    public class UpdatePermissionDto
+    {
+        [Required(ErrorMessage = "UserName is required")]
+        public string UserName { get; set; }
+
+    }
+}
